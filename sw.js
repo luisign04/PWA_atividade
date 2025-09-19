7124777680395e7b58133dbf22d75d3dad484e6b
Luis Augusto <oluisaugusto04@gmail.com>
2025-09-19 14:14:13 -0300

    alteração_no_manifest e service worker

--- sw.js
+++ sw.js
@@ -4,8 +4,8 @@ const urlsToCache = [
   '/index.html',
   '/styles.css',
   '/app.js',
-  '/icon-192x192.png',
-  '/icon-512x512.png'
+  './icons/icon-192x192.png',
+  './icons/icon-512x512.png',
 ];
 
 self.addEventListener('install', function(event) {
